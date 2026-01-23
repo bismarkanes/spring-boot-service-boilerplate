bc54934c61a4c23cfbef98ec38095994175012d6
bismarkanes <bismark.john.anes@gmail.com>
2026-01-23 09:53:14 +0700

    Rename app test

--- src/test/java/com/bismark/serviceboilerplate/ServiceApplicationTests.java
+++ src/test/java/com/bismark/serviceboilerplate/ServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bismark.basic_spring_security;
+package com.bismark.serviceboilerplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicSpringSecurityApplicationTests {
+class ServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
